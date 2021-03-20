8670059463eeb8cdf048f5fcae6cf76fbf04965d
Martin Fleming <78125388+martinflemingdev@users.noreply.github.com>
2021-03-20 18:52:58 -0400

    Update TwitterBot.java

--- java/TwitterBot.java
+++ java/TwitterBot.java
@@ -18,37 +18,39 @@ public class TwitterBot {
 			
 			 //everyday at 8AM and 8PM
 			
-			if ((LocalTime.now().getHour() == 8  ||
+			if ((LocalTime.now().getHour() == 8   ||
 			     LocalTime.now().getHour() == 20) &&
 			     LocalTime.now().getMinute() == 0 &&
 			     LocalTime.now().getSecond() == 0 &&
-			     LocalTime.now().getNano() > 900000000 )
-			     {
-				NewsService news = new NewsService();
-				NewsObject todaysNews = news.getNews();
-				Article[] articles = todaysNews.getArticles();
+			     LocalTime.now().getNano() > 900000000 ) {
+				try {
+					NewsService news = new NewsService();
+					NewsObject todaysNews = news.getNews();
+					Article[] articles = todaysNews.getArticles();
 				
-				int i;
-				for (i=0; i<articles.length; i++) {
+					int i;
+					for (i=0; i<articles.length; i++) {
 					
 					// due to only tweeting sports news, added condititional to 
 					// filter out headlines specific to sports teams and last title
 					
-					if (!articles[i].getTitle().contains(title)		 &&
-						!articles[i].getTitle().contains("Phillies") 	 &&
-						!articles[i].getTitle().contains("Flyers")	 &&
-						!articles[i].getTitle().contains("Sixers")  	 &&
-						!articles[i].getTitle().contains("76ers")	 &&
-						!articles[i].getTitle().contains("Eagles") ) 
-						{
-						break;
+						if (!articles[i].getTitle().contains(title)	 &&
+						    !articles[i].getTitle().contains("Phillies") &&
+						    !articles[i].getTitle().contains("Flyers")	 &&
+						    !articles[i].getTitle().contains("Sixers")   &&
+						    !articles[i].getTitle().contains("76ers")	 &&
+						    !articles[i].getTitle().contains("Eagles") ) {
+						    break;
+						}
 					}
+				
+					title = articles[i].getTitle();
+					url = articles[i].getUrl();
+					newTweet(title + "\n" + url);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-				
-				title = articles[i].getTitle();
-				url = articles[i].getUrl();
-				
-				newTweet(title + "\n" + url);
 			}
 		}
     }
