c945c1f23cceef34126c6fca4b1877c3505e93c4
Martin Fleming <78125388+martinflemingdev@users.noreply.github.com>
2021-03-21 21:09:41 -0400

    Update NewsObject.java

--- java/NewsObject.java
+++ java/NewsObject.java
@@ -13,20 +13,13 @@ public class NewsObject {
 	public String getStatus() {
 		return status;
 	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
+
 	public int getTotalResults() {
 		return totalResults;
 	}
-	public void setTotalResults(int totalResults) {
-		this.totalResults = totalResults;
-	}
+
 	public Article[] getArticles() {
 		return articles;
-	}
-	public void setArticles(Article[] articles) {
-		this.articles = articles;
 	}
 	
 }
